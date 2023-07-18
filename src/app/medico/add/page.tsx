33502156d63edb43f12b5d3c2dbfa80e77bcfdc0
Segundo <pantoja2187@gmail.com>
2023-07-17 21:59:55 -0500

    DEV:end:Medico front v.0.0.0.2

---
+++ src/app/medico/add/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import MedicoForm from "@/app/components/MedicoForm";
+export default function MedicoAddPage() {
+  return (
+    <div className="container">
+      <h1>Crear Medico</h1>
+      <MedicoForm />
+    </div>
+  );
+}
